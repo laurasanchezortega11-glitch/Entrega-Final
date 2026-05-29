f6837a1397d1e62652d83cfd3d21771355ad58d9
Laura Sanchez <laurasanchezortega11@gmail.com>
2026-05-28 21:19:13 -0500

    Initial pipeline structure

---
+++ pipeline/main.py
@@ -0,0 +1,5 @@
+from protein import Protein
+
+p1 = Protein("P00519")
+
+print(p1.uniprot_id)
---
+++ pipeline/protein.py
@@ -0,0 +1,6 @@
+class Protein:
+
+    def __init__(self, uniprot_id):
+
+        self.uniprot_id = uniprot_id
+        
